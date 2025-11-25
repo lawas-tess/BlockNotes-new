a49afa28695ecacd9284ec71ff07c97c2f44b139
Jose Raphael Lawas <joseraphaellawas@gmail.com>
2025-11-25 23:13:33 +0800

    For deployment

--- note_app/note_app/settings.py
+++ note_app/note_app/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Blocknotes',
         'USER': 'root',
-        'PASSWORD': 'dblapuredemo123',
+        'PASSWORD': '#Admin_12345',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "set sql_mode='strict_trans_tables'"},
